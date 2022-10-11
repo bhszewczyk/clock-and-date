52c2116dea662d95782078617b0a04c7a3c0b61a
Barbara Szewczyk <comamysza@gmail.com>
2022-10-11 21:13:08 +0200

    Add time and date update every second

--- script.js
+++ script.js
@@ -33,3 +33,31 @@ toggleThemeBtn.addEventListener('click', (e) => {
 	const html = document.querySelector('html');
 	html.classList.toggle('dark');
 });
+
+function setTime() {
+	const time = new Date();
+	const month = time.getMonth();
+	const day = time.getDate();
+	const weekDay = time.getDay();
+	const hours = time.getHours();
+	const minutes = time.getMinutes();
+	const seconds = time.getSeconds();
+
+	hourEl.style.transform = `translate(-50%, -100%) rotate(${
+		(360 / 12) * hours
+	}deg)`;
+	minEl.style.transform = `translate(-50%, -100%) rotate(${
+		(360 / 60) * minutes
+	}deg)`;
+	secEl.style.transform = `translate(-50%, -100%) rotate(${
+		(360 / 60) * seconds
+	}deg)`;
+
+	timeEl.innerText = `${hours < 10 ? '0' + hours : hours}:${
+		minutes < 10 ? '0' + minutes : minutes
+	}:${seconds < 10 ? '0' + seconds : seconds}`;
+	dateEl.innerText = `${days[weekDay]}, ${months[month]}`;
+	dateEl.innerHTML += `<span class="circle">${day}</span>`;
+}
+setTime();
+setInterval(setTime, 1000);
